15eede47b87e995325441e4cffe0882a1dd4ea2f
Daniel Rodriguez <technoveltyco@users.noreply.github.com>
2023-02-15 10:56:05 +0000

    Add starter code

---
+++ index.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from 'path';
+import inquirer from "inquirer";
+import generateMarkdown from "./utils/generateMarkdown";
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
